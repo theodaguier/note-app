ac72e0b1279bec9adc6c276246e0f4d4ae3aafb4
theodaguier <92515854+theodaguier@users.noreply.github.com>
2022-08-23 13:53:17 +0200

    Notes component

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,18 +1,38 @@
+import { useState } from 'react';
 import { GrClose } from 'react-icons/gr';
+import { BsChevronDown } from 'react-icons/bs';
+import Draggable from 'react-draggable';
 
 const Note = ({ id, text, date, handleDeleteNote }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(true);
+  };
+
   return (
-    <div className="note">
-      <div className="note-data">
-        <span>{text}</span>
-        <span className="date">{date}</span>
-        <GrClose
-          onClick={() => handleDeleteNote(id)}
-          className="delete-icon"
-          size="1.3em"
-        />
+    <Draggable>
+      <div className="note">
+        <div className="note-header">
+          <BsChevronDown
+            className="hidden-icon"
+            size="1.3em"
+            onClick={() => setToggle()}
+          />
+        </div>
+        <div className="note-data">
+          <span>{text}</span>
+          <GrClose
+            onClick={() => handleDeleteNote(id)}
+            className="delete-icon"
+            size="1.3em"
+          />
+        </div>
+        <div className="note-footer">
+          <span className="date">{date}</span>
+        </div>
       </div>
-    </div>
+    </Draggable>
   );
 };
 
